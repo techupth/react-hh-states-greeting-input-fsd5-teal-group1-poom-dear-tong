bcd4472333e12ed4ee0255d0f70838fa080d9197
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-12-21 14:10:46 +0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
+  const [inputValue, setInputValue] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    setMessage(inputValue);
+    setInputValue("");
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
-      <div className="input-container">
-        <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
-      </div>
+      <div className="greeting-container">{message}</div>
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="greeting-message">New Greeting Message</label>
+          <input
+            id="greeting-message"
+            type="text"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+        </div>
 
-      <div className="buttons">
-        <button>Update text</button>
-      </div>
+        <div className="buttons">
+          <button type="submit">Update text</button>
+        </div>
+      </form>
     </div>
   );
 }
